8ddd94bc2766209fa33ee96fae4e5ae733a754af
Oğuzhan Denizhan <oguzhandenizhan25@gmail.com>
2022-02-11 23:45:08 +0300

    vize final notlarının ortalamasını hesaplama
    C# 'da İki boyutlu dizi kullanılarak Vize ve final  notları kullanıcıdan alınıp ortalamasını hesaplama programı.

---
+++ ortSınıf/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ortSınıf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] dizi = new double[3, 3];
+
+            for (int i = 0; i <= 2; i++)
+            {
+                Console.WriteLine((i+1) + ". Öğrenci ");
+                for (int j = 0; j < 2; j++)
+                {
+                   double temp = Convert.ToDouble(Console.ReadLine());
+                    dizi[i, j] = temp;
+                }
+
+                dizi[i, 2] = ( 0.4 * dizi[i, 0] + 0.6 * dizi[i, 1]);
+                Console.WriteLine("Ortalama: "+dizi[i, 2]);
+
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
